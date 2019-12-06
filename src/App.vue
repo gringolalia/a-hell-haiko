b94ed4c15c8fa69d1e0a39f80e17774ec4be58e3
Colin Brayton <gringolalia@gmail.com>
2019-12-06 17:14:21 -0300

    inkline and thinkers

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+#app
+	i-navbar
+		i-navbar-brand(to="/") 
+			i.fas.fa-sun.fa-2x._margin-right-1
+			| Engenho Novo LLP
+		i-navbar-items
+			i-nav
+				i-nav-item(:to='{ name: \'index\' }') Home
+				i-nav-item(:to='{ name: \'about\' }') About
+			i-nav
+				i-input.item(placeholder='Type something..')
+					i-button(variant='primary' slot='append')
+						font-awesome-icon(icon='search').
+								
+								
+	router-view
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: 'News Cycle', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+i-navbar > i-navbar-brand {
+	font-siz: 25px;
 }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+.hello
+	h1
+		i.fas.fa-chess.fa-4x
+	h1._margin-bottom-1 Vue Thru Tinted Window Glass
+	h2._margin-top-0 Addio Mondo Crudele
+	h3._margin-top-0 Inkline and Thinkers
+	._display-flex._flex-direction-row
+		i-button._margin-right-1(variant='primary') Prime Rib
+		i-button(variant='dark') Second Hand
+
 </template>
+
+<style>.hello {
+	min-height: calc(100vh - 75px);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+</style>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+	name: 'home',
+	components: {
+		HelloWorld
+	}
 }
 </script>
