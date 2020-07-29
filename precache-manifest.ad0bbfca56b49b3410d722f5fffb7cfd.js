3b66c21763c0f215d18fce24e4cca1768ef72e3c
Colin Brayton <gringolalia@gmail.com>
2020-07-29 19:24:34 -0300

    deploy

---
+++ precache-manifest.ad0bbfca56b49b3410d722f5fffb7cfd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee39c992c7183dce705b",
+    "url": "/a-hell-haiku/js/about.39c1c059.js"
+  },
+  {
+    "revision": "c3e99500d79cfdb94d88",
+    "url": "/a-hell-haiku/css/app.ca8633be.css"
+  },
+  {
+    "revision": "c3e99500d79cfdb94d88",
+    "url": "/a-hell-haiku/js/app.b9447151.js"
+  },
+  {
+    "revision": "b5fee9fa0830ff1e11d9",
+    "url": "/a-hell-haiku/css/chunk-vendors.4da833ed.css"
+  },
+  {
+    "revision": "b5fee9fa0830ff1e11d9",
+    "url": "/a-hell-haiku/js/chunk-vendors.d9a6a195.js"
+  },
+  {
+    "revision": "ebe25a09f8c6844b928dae1ffa65ac79",
+    "url": "/a-hell-haiku/fonts/inkline.ebe25a09.ttf"
+  },
+  {
+    "revision": "20a42ef12aac41cac1c8969c5758948e",
+    "url": "/a-hell-haiku/fonts/inkline.20a42ef1.woff"
+  },
+  {
+    "revision": "ff99d3fb75eedf667974b02c42e391d2",
+    "url": "/a-hell-haiku/fonts/inkline.ff99d3fb.eot"
+  },
+  {
+    "revision": "abe51263a52de521c9e3e8d8c90f8ac4",
+    "url": "/a-hell-haiku/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/a-hell-haiku/robots.txt"
+  }
+];
